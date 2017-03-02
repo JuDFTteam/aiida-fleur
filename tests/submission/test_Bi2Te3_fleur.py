82064bd3643f10f2fc709b3464b46d326f299fb6
J.Broeder <j.broeder@fz-juelich.de>
2017-03-02 12:23:29 +0100

    restructureing of tests, added basic Fleur tests, to test if FleurinpData and the Fleurparser work with the features of Fleur

---
+++ tests/submission/test_Bi2Te3_fleur.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env runaiida
+# -*- coding: utf-8 -*-
+
+__copyright__ = (u"Copyright (c), 2016, Forschungszentrum Jülich GmbH, "
+                 "IAS-1/PGI-1, Germany. All rights reserved.")
+__license__ = "MIT license, see LICENSE.txt file"
+__version__ = "0.27"
+__contributors__ = "Jens Broeder"
+
+from aiida import load_dbenv, is_dbenv_loaded
+if not is_dbenv_loaded():
+    load_dbenv()
+
+import sys
+import os
+
+from aiida.common.example_helpers import test_and_get_code
+from aiida.orm import DataFactory
+
+# If set to True, will ask AiiDA to run in serial mode (i.e., AiiDA will not
+# invoke the mpirun command in the submission script)
+run_in_serial_mode = True#False
+
+################################################################
+
+
+ParameterData = DataFactory('parameter')
+StructureData = DataFactory('structure')
+FleurinpData = DataFactory('fleurinp')
+try:
+    dontsend = sys.argv[1]
+    if dontsend == "--dont-send":
+        submit_test = True
+    elif dontsend == "--send":
+        submit_test = False
+    else:
+        raise IndexError
+except IndexError:
+    print >> sys.stderr, ("The first parameter can only be either "
+                          "--send or --dont-send")
+    sys.exit(1)
+
+try:
+    codename = sys.argv[2]
+except IndexError:
+    codename = None
+
+queue = None
+# queue = "th1_small"
+settings = None
+#####
+
+code = test_and_get_code(codename, expected_code_type='fleur_inp.fleur')
+
+#TODO: how to make smart path?
+# get where tests folder is, then relative path
+inpxmlfile = '/usr/users/iff_th1/broeder/aiida/github/aiida_fleur_plugin/tests/inp_xml_files/Bi2Te3XML/files/inp.xml'
+enpara = '/usr/users/iff_th1/broeder/aiida/github/aiida_fleur_plugin/tests/inp_xml_files/Bi2Te3XML/files/enpara'
+
+fleurinp = FleurinpData(files = [inpxmlfile, enpara])
+print fleurinp.files
+## For remote codes, it is not necessary to manually set the computer,
+## since it is set automatically by new_calc
+#computer = code.get_remote_computer()
+#calc = code.new_calc(computer=computer)
+
+calc = code.new_calc()
+calc.label = "Bi2Te3 Fleur test"
+calc.description = ("Simple test of Fleur with one step:"
+                    "1.Generate a starting density and run a single iteration "
+                    "and test for ef, total energy and orbital moment. Uses: SOC,LOs")
+calc.set_max_wallclock_seconds(5 * 60)  # 5 min
+# Valid only for Slurm and PBS (using default values for the
+# number_cpus_per_machine), change for SGE-like schedulers
+calc.set_resources({"num_machines": 1})
+if run_in_serial_mode:
+    calc.set_withmpi(False)
+## Otherwise, to specify a given # of cpus per machine, uncomment the following:
+# calc.set_resources({"num_machines": 1, "num_mpiprocs_per_machine": 8})
+
+#calc.set_custom_scheduler_commands("#SBATCH --account=ch3")
+
+if queue is not None:
+    calc.set_queue_name(queue)
+
+calc.use_fleurinpdata(fleurinp)
+#calc.use_code(code)
+
+if settings is not None:
+    calc.use_settings(settings)
+
+
+if submit_test:
+    subfolder, script_filename = calc.submit_test()
+    print "Test_submit for calculation (uuid='{}')".format(
+        calc.uuid)
+    print "Submit file in {}".format(os.path.join(
+        os.path.relpath(subfolder.abspath),
+        script_filename
+    ))
+else:
+    calc.store_all()
+    print "created calculation; calc=Calculation(uuid='{}') # ID={}".format(
+        calc.uuid, calc.dbnode.pk)
+    calc.submit()
+    print "submitted calculation; calc=Calculation(uuid='{}') # ID={}".format(
+        calc.uuid, calc.dbnode.pk)
+
--- tests/test_submit/test_fleur.py
+++ tests/test_submit/test_fleur.py
@@ -2,10 +2,10 @@
 from aiida import load_dbenv, is_dbenv_loaded
 if not is_dbenv_loaded():
     load_dbenv()
-import sys,os 
+#import sys,os 
 
 from aiida.orm import Calculation, Code, Computer, Data, Node, load_node
-from aiida.orm import CalculationFactory, DataFactory
+from aiida.orm import DataFactory
 
 StructureData = DataFactory('structure')
 ParameterData = DataFactory('parameter')
@@ -14,7 +14,9 @@ ParameterData = DataFactory('parameter')
 # Set your values here
 codename1 = 'inpgen_iff@local_iff'#'inpgen_mac_30_11_2016@local_mac'
 #codename2 = 'fleur_iff@local_iff'#'fleur_mac_v0_27@local_mac'
-codename = 'fleur_iff003_v0_27@iff003'
+#codename = 'fleur_iff003_v0_27@iff003'
+codename = 'fleur_MPI_iff003_v0_27@iff003'
+
 ###############################
 
 fleurinppk = 4968#Si
@@ -30,8 +32,8 @@ calc.use_fleurinpdata(fleurinpd)
 calc.use_code(code)
 calc.set_withmpi(False)
 calc.set_queue_name('th123_node')
-calc.submit_test()
-#calc.store_all()
-#calc.submit()
+#calc.submit_test()
+calc.store_all()
+calc.submit()
 print "created calculation,{} with PK={}".format(calc.label,calc.pk)
 pkid_parent = calc.pk
