228a13e21f7bb133e44841f625a6638bce011391
broeder <j.broeder@fz-juelich.de>
2017-03-27 16:25:42 +0200

    added setup.json

--- setup.py
+++ setup.py
@@ -1,36 +1,12 @@
-__version__ = "0.1"
-__authors__ = "Jens Broeder"
-
 from setuptools import setup, find_packages
-
-setup(
-    name='aiida-fleur',
-    version='0.1',
-    url='http://www.flapw.de', # on bitbucket
-    license='MIT license, see LICENSE.txt',
-    description='The AiiDA plugin for the all-electron DFT code FLEUR (www.flapw.de)',
-    author = 'Jens Broeder',
-    author_email='j.broeder@fz-juelich.de',
-    #scripts=['helloworld'],
+import json
 
 
-    install_requires=[
-        'lxml == 3.6.4',
-        'aiida>=0.7.0',
-        #'fleurinpdata>='
-        'ase',
-        ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    ],
-    packages=["aiida_fleur"],# find_packages(),
-    entry_points={
-            'aiida.calculations':['fleur.fleur = aiida_fleur.calculations.fleur:FleurCalculation', 'fleur.inpgen = aiida_fleur.FleurinputgenCalculation'],
-            'aiida.parsers':['fleur.fleurparser = aiida_fleur.parsers.fleur:FleurParser', 'fleur.inpgenparser = aiida_fleur.parsers.fleur_inpgen:FleurinputgenParser'],
-            #'aiida.data':['aiida_fleur.fleurinp = fleuplugin.data'],
-            #'aiida.tools':['aiida_fleur.tools = aiida_fleur.tools'],
-            }
-)
+if __name__ == '__main__':
+    with open('setup.json', 'r') as info:
+        kwargs = json.load(info)
+    setup(
+        include_package_data=True,
+        packages=find_packages(),
+        **kwargs
+    )
