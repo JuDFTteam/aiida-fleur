3f5bd0f980cb9ccd1a711d921dbc1f6b647cde7c
broeder <j.broeder@fz-juelich.de>
2017-05-18 13:39:54 +0200

    rest from renaming docs and tests

--- tests/util/test_read_cif_folder.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from aiida import load_dbenv, is_dbenv_loaded
-if not is_dbenv_loaded():
-    load_dbenv()
-from aiida.tools.codespecific.fleur.read_cif_folder import read_cif_folder
-
-
-read_cif_folder(log=True, store=True, rekursive=True, extras={'type' : 'bulk', 'project': 'Fusion', 'specification' : 'aiida_work', 'comment' : 'Materials for Fusion'})
