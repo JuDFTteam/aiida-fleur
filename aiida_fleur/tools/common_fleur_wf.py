8a19201dea596fe0fda74d3d62b59fc7d86e4419
broeder <j.broeder@fz-juelich.de>
2017-08-25 14:31:08 +0200

    added some methods to calculte the convex hull, formation energies and methods to retrieve information out of the chemical formula to tools/common_fleur_wf

--- aiida_fleur/tools/common_fleur_wf.py
+++ aiida_fleur/tools/common_fleur_wf.py
@@ -114,7 +114,23 @@ def get_inputs_fleur(code, remote, fleurinp, options, label='', description='', 
     
     if settings:
         inputs.settings = settings
-        
+    
+    '''
+    options = {
+    "max_wallclock_seconds": int,
+    "resources": dict,
+    "custom_scheduler_commands": unicode,
+    "queue_name": basestring,
+    "computer": Computer,
+    "withmpi": bool,
+    "mpirun_extra_params": Any(list, tuple),
+    "import_sys_environment": bool,
+    "environment_variables": dict,
+    "priority": unicode,
+    "max_memory_kb": int,
+    "prepend_text": unicode,
+    "append_text": unicode}
+    '''
     return inputs
 
 
@@ -153,3 +169,172 @@ def get_inputs_inpgen(structure, inpgencode, options, label='', description='', 
     inputs._options.resources = {"num_machines": 1}
     #print(inputs)
     return inputs
+    
+
+def get_natoms_element(formula):
+    """
+    Converts 'Be24W2' to {'Be': 24, 'W' : 2}, also BeW to {'Be' : 1, 'W' : 1}
+    """
+    
+    import re
+    elem_count_dict = {}
+    elements = re.findall('[A-Z][^A-Z]*', formula)
+    #re.split('(\D+)', formula)
+    
+    for i, elm in enumerate(elements):
+        elem_count = re.findall('\d+|\D+', elm)
+        #print(elem_count)
+        if len(elem_count) == 1:
+            elem_count_dict[elem_count[0]] = 1
+        else:
+            elem_count_dict[elem_count[0]] = float(elem_count[1])
+            
+    return elem_count_dict
+
+# test
+#get_natoms_element('BeW')
+#get_natoms_element('Be2W')
+
+def get_atomprocent(formula):
+    """
+    This converts a formula to a dictionary with elemnt : atomprocent 
+    example converts 'Be24W2' to {'Be': 24/26, 'W' : 2/26}, also BeW to {'Be' : 0.5, 'W' : 0.5}    
+    :params: formula: string
+    :returns: a dict, element : atomprocent
+
+    # Todo alternative with structuredata
+    """
+    form_dict_new = {}
+    form_dict = get_natoms_element(formula)
+    ntotal = sum(form_dict.values())
+    for key, val in form_dict.iteritems():
+        val_new = float(val)/ntotal
+        form_dict_new[key] = val_new
+    return form_dict_new
+
+# test
+
+def get_weight_procent(formula):
+    """
+    This converts a formula to a dictionary with elemnt : weightprocent 
+    example converts 'Be24W2' to {'Be': , 'W' : }, also BeW to {'Be' : , 'W' : }    
+    :params: formula: string
+    :returns: a dict, element : weightprocent
+
+    # Todo alternative with structuredata
+    """
+    
+    pass
+
+
+def determine_formation_energy(struc_te_dict, ref_struc_te_dict):
+    """
+    This method determines the formation energy. 
+    E_form =  E(A_xB_y) - x*E(A) - y*E(B)
+    
+    :inputs: struc_te_dict: python dictionary in the form of {'formula' : total_energy} for the compound(s)
+    :inputs: ref_struc_te_dict: python dictionary in the form of {'formula' : total_energy per atom} for the elements
+    :returns: list of floats, dict {formula : eform, ..} units energy/per atom, energies have some unit as energies given
+    """
+    eform_list = []
+    eform_dict = {} 
+    ref_el = ref_struc_te_dict.keys()
+    
+    for formula, tE in struc_te_dict.iteritems():
+        elements_count = get_natoms_element(formula)
+        ntotal = float(sum(elements_count.values()))
+        eform = tE#abs(tE)
+        for elem, count in elements_count.iteritems():
+            if elem in ref_el:
+                eform = eform - count * ref_struc_te_dict.get(elem)#abs(ref_struc_te_dict.get(elem))
+            else:
+                print('Reference energy missing for element {}.'
+                      'You need to provide reference energies for all elements in you compound.'
+                       ''.format(elem))
+        eform_dict[formula] = eform/ntotal
+        eform_list.append(eform/ntotal)
+    return eform_list, eform_dict    
+
+# test
+#determine_formation_energy({'BeW' : 2, 'Be2W' : 2.5}, {'Be' : 1, 'W' : 1})
+
+def determine_convex_hull(formation_en_grid):
+    """
+    Wraps the scipy.spatial ConvexHull algo for our purposes.
+    For now only for 2D phase diagrams
+    Adds the points [1.0, 0.0] and [0.0, 1.0], because in material science these
+    are always there.
+    
+    :params: formation_en_grid: list of points in phase space [[x, formation_energy]]
+    :returns: a hul datatype
+    """
+    import numpy as np
+    from scipy.spatial import ConvexHull
+    
+    # TODO multi d
+    # check if endpoints are in
+    if [1.0, 0.0] not in formation_en_grid:
+        formation_en_grid.append([1.0, 0.0])
+    if [0.0, 0.0] not in formation_en_grid:
+        formation_en_grid.append([0.0, 0.0])   
+     
+    points = np.array(formation_en_grid)
+    hull = ConvexHull(points)
+    
+    return hull
+    
+    
+def get_scheduler_extras(code, resources, extras={}, project='jara0043'):
+    """
+    This is a utilty function with the goal to make prepare the right resource and scheduler extras for a given computer.
+    Since this is user dependend you might want to create your own.
+
+    return: dict, custom scheduler commands
+    """
+    nnodes = resources.get('num_machines', 1)
+    
+    memp_per_node = 125000# max recommend 126000 MB on claix jara-clx nodes
+    if not extras:
+        # use defaults # TODO add other things, span, pinnning... openmp
+        extras = {'lsf' : '#BSUB -P {} \n#BSUB -M {}  \n#BSUB -a intelmpi'.format(project, memp_per_node*nnodes),#{'-P' : 'jara0043', '-M' : memp_per_node*nnodes, '-a' : 'intelmpi'},
+                 'torque' : '',#{},
+                 'direct' : ''}#{}}
+    
+    # get the scheduler type from the computer the code is run on.
+    com = code.get_computer()
+    #com_name = com.get_name()
+    scheduler_type = com.get_scheduler_type()
+     
+    default_per_machine = com.get_default_mpiprocs_per_machine()
+    if not default_per_machine:
+        default_per_machine = 24# claix, lsf does can not have default mpiprocs... #TODO this better
+    tot_num_mpiprocs = resources.get('tot_num_mpiprocs', default_per_machine*nnodes)
+    
+    if scheduler_type == 'lsf':
+        new_resources = {'tot_num_mpiprocs' : tot_num_mpiprocs}# only this needs to be given
+    elif scheduler_type == 'torque':
+        new_resources = resources#{'num_machines', 1} # on iff003 currently we do not do multinode mpi, 
+        #like this it will get stuck on iff003
+    else:
+        new_resources = resources
+    scheduler_extras = extras.get(scheduler_type, '')
+
+    return new_resources, scheduler_extras
+
+
+#test
+###############################
+#codename = 'inpgen@local_mac'#'inpgen_v0.28@iff003'#'inpgen_iff@local_iff'
+#codename2 = 'fleur_v0.28@iff003'#'fleur_mpi_v0.28@iff003'# 'fleur_iff_0.28@local_iff''
+#codename2 = 'fleur_max_1.3_dev@iff003'
+#codename2 = 'fleur_mpi_max_1.3_dev@iff003'
+#codename4 = 'fleur_mpi_v0.28@claix'
+###############################
+#code = Code.get_from_string(codename)
+#code2 = Code.get_from_string(codename2)
+#code4 = Code.get_from_string(codename4)
+#print(get_scheduler_extras(code, {'num_machines' : 1}))
+#print(get_scheduler_extras(code2, {'num_machines' : 2}))
+#print(get_scheduler_extras(code4, {'num_machines' : 1}))
+
+    
