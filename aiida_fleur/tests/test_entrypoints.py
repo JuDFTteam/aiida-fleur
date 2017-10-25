a156d9170a66a89663bce483ae4ba444a0a2ef53
broeder <j.broeder@fz-juelich.de>
2017-10-25 11:22:11 +0200

    moved fixtures to conftest

--- aiida_fleur/tests/test_entrypoints.py
+++ aiida_fleur/tests/test_entrypoints.py
@@ -2,22 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import pytest
-#from helpers import aiida_profile
-#from aiida.utils.fixtures import fixture_manager
-
-@pytest.fixture(scope='session')
-def aiida_profile():
-    from aiida.utils.fixtures import fixture_manager
-    with fixture_manager() as fixture_mgr:
-        fixture_mgr.create_profile()
-        yield fixture_mgr
-
-@pytest.fixture(scope='function')
-def test_data(aiida_profile):
-    # load my test data
-    yield
-    aiida_profile.reset_db()
+#    aiida_profile.reset_db()
 #    
 #def test_my_stuff(test_data):
 #   # run a test
