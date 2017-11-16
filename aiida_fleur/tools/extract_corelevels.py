c04e8d3ffed0f53ca8b0ad7ff9b35110f61ea5f1
broeder <j.broeder@fz-juelich.de>
2017-11-16 13:00:04 +0100

    extract_corelevels also parses now the electron config information

--- aiida_fleur/tools/extract_corelevels.py
+++ aiida_fleur/tools/extract_corelevels.py
@@ -113,8 +113,14 @@ def extract_corelevels(outxmlfile, options=None):
     species_xpath = '/fleurOutput/inputData/atomSpecies'
     iteration_xpath = '/fleurOutput/scfLoop/iteration'
     atomgroup_xpath = '/fleurOutput/inputData/atomGroups'
+    coreconfig_xpath = 'electronConfig/coreConfig/text()'
+    valenceconfig_xpath = 'electronConfig/valenceConfig/text()'
+    state_occ_xpath = 'electronConfig/stateOccupation'
+    
     relcoreStates_xpath = 'coreStates'
-
+    relpos_xpath = 'relPos'
+    abspos_xpath = 'absPos'
+    filmpos_xpath = 'filmPos'
     #TODO all the attribute names...
     ######################
 
@@ -140,7 +146,6 @@ def extract_corelevels(outxmlfile, options=None):
         try:
             tree = etree.parse(outxmlfile, parser)
         except XMLSyntaxError:
-            #print 'here'
             parser_info['parser_warnings'].append('Skipping the parsing of the xml file. Repairing was not possible.')
             parse_xml = False
 
@@ -151,8 +156,7 @@ def extract_corelevels(outxmlfile, options=None):
     # 2. get all species from input
     # get element, name, coreStates
     # TODO why can this not be eval_xpath2?
-    species_nodes = eval_xpath(root, species_xpath, parser_info)#/fleurinp/
-    #print species_nodes
+    species_nodes = eval_xpath(root, species_xpath, parser_info)
     species_atts = {}
     species_names = []
     for species in species_nodes:
@@ -161,20 +165,35 @@ def extract_corelevels(outxmlfile, options=None):
         species_element = species.get('element')
         species_atomicnumber = species.get('atomicNumber')
         species_magMom = species.get('magMom')
+        #TODO sometimes not in inp.xml... what if it is not there
+        coreconfig = eval_xpath(species, coreconfig_xpath, parser_info)
+        valenceconfig = eval_xpath(species, valenceconfig_xpath , parser_info)
+        state_occ = eval_xpath2(species, state_occ_xpath, parser_info)
+        
+        #parse state occ
+        state_results = []
+        for tag in state_occ:#always a list?
+             state = tag.get('state')
+             spinUp = tag.get('spinUp')
+             spinDown = tag.get('spinDown')
+             state_results.append({state : [spinUp, spinDown]})
+
+        
         species_atts[species_name] = {'name' : species_name,
                                       'corestates' : species_corestates,
                                       'element': species_element,
                                       'atomgroups' : [],
                                       'mag_mom' : species_magMom,
-                                      'atomic_number' : species_atomicnumber}
+                                      'atomic_number' : species_atomicnumber,
+                                      'coreconfig' : coreconfig,
+                                      'valenceconfig' : valenceconfig,
+                                      'stateOccupation' : state_results}
         species_names.append(species_name)
-    #nspecies = len(species_nodes)
-    #print(species_atts)
+
     #3. get number of atom types and their species from input
     atomtypes = []
     atomgroup_nodes = eval_xpath(root, atomgroup_xpath, parser_info)#/fleurinp/
-    #print atomgroup_nodes
-    #print parser_info
+    # always a list?
     for atomgroup in atomgroup_nodes:
         types_dict = {}
         group_species = atomgroup.get('species')
@@ -182,17 +201,20 @@ def extract_corelevels(outxmlfile, options=None):
             species_atts[group_species]['atomgroups'].append(atomgroup)
             element = species_atts[group_species]['element']
             atomicnumber = int(species_atts[group_species]['atomic_number'])
-            #TODO get coreconfig,..., usually not in inp.xml...
+            coreconf = species_atts[group_species]['coreconfig']
+            valenceconf = species_atts[group_species]['valenceconfig']
+            stateocc = species_atts[group_species]['stateOccupation']
+            a = eval_xpath2(atomgroup, relpos_xpath, parser_info) + eval_xpath2(atomgroup, abspos_xpath, parser_info) + eval_xpath2(atomgroup, filmpos_xpath, parser_info)# always list
+            natoms = len(a)
             types_dict = {'species' : group_species, 'element' : element,
-                          'atomic_number' : atomicnumber, 'coreconfig': '',
-                          'valenceconfig' : '',
-                          'stateOccupation' : []}
+                          'atomic_number' : atomicnumber, 'coreconfig': coreconf,
+                          'valenceconfig' : valenceconf,
+                          'stateOccupation' : stateocc,
+                          'natoms' : natoms}
         atomtypes.append(types_dict)
 
-    #print atomtypes
-    natomgroup = len(atomgroup_nodes)
-    #print natomgroup#, nspecies
-    #print species_names
+    #natomgroup = len(atomgroup_nodes)
+    #print(natomgroup)
     corelevels = []
 
     #4 get corelevel dimension from atoms types.
@@ -200,32 +222,24 @@ def extract_corelevels(outxmlfile, options=None):
     #6 parse corelevels:
 
     iteration_nodes = eval_xpath2(root, iteration_xpath, parser_info)
-    #print iteration_nodes
     nIteration = len(iteration_nodes)
     if nIteration >= 1:
         iteration_to_parse = iteration_nodes[-1]#TODO:Optional all or other
         #print iteration_to_parse
         corestatescards = eval_xpath2(iteration_to_parse, relcoreStates_xpath, parser_info)
         # maybe does not return a list...
-        #print(corestatescards)
         for type in atomtypes: # spin=2 is already in there
             corelevels.append([])
 
         for corestatescard in corestatescards:
-            #print('here')
-            #print(etree.tostring(corestatescard, pretty_print=True))
-            #print(corestatescard, iteration_to_parse, parser_info)
             corelv = parse_state_card(corestatescard, iteration_to_parse, parser_info)
-            #print(corelv['atomtype'])
-            #print corelv
-            #print(corelv['atomtype'])
-            corelevels[int(corelv['atomtype'])-1].append(corelv)# is corelv['atomtype'] always an integer
-            #corelevels.append(corelv)
+            corelevels[int(corelv['atomtype'])-1].append(corelv)# is corelv['atomtype'] always an integer?
 
     #print parser_info
     #pprint(corelevels[0][1]['corestates'][2]['energy'])
     #corelevels[atomtypeNumber][spin]['corestates'][corestate number][attribute]
     return corelevels, atomtypes
+
 
 def parse_state_card(corestateNode, iteration_node, parser_info={'parser_warnings' : []}):
     """
@@ -261,7 +275,6 @@ def parse_state_card(corestateNode, iteration_node, parser_info={'parser_warning
 
     kinEnergy = get_xml_attribute(corestateNode, kinEnergy_name, parser_info)
     vE2, suc = convert_to_float(kinEnergy, parser_info)
-    #print('kinEnergy {}'.format(kinEnergy))
     eigenvalueSum = get_xml_attribute(corestateNode, eigenvalueSum_name, parser_info)
     vE2, suc = convert_to_float(eigenvalueSum, parser_info)
 
@@ -278,7 +291,7 @@ def parse_state_card(corestateNode, iteration_node, parser_info={'parser_warning
     # parse each core state #Attention to spin
     states = []
     corestates = eval_xpath2(corestateNode, state_xpath)#, parser_info)
-    #print('corestates {}'.format(corestates))
+
     for corestate in corestates:# be careful that corestates is a list
         state_dict = {}
         n_state = get_xml_attribute(corestate, n_name, parser_info)
@@ -289,31 +302,11 @@ def parse_state_card(corestateNode, iteration_node, parser_info={'parser_warning
         state_dict = {'n' : n_state, 'l' : l_state, 'j' : j_state, 'energy' : energy, 'weight' : weight}
         states.append(state_dict)
 
-    #print(states)
-
     core_states = {'eigenvalue_sum' : eigenvalueSum, 'corestates': states, 'spin' : spin, 'kin_energy' : kinEnergy, 'atomtype' : atomtype}
-    #pprint(core_states)
     return core_states
-'''
-def eval_xpath(node, xpath):
-    """
-    Tries to evalutate an xpath expression. If it fails it logs it.
 
-    :param root node of an etree and an xpath expression (relative, or absolute)
-    :returns either nodes, or attributes, or text
-    """
-    try:
-        return_value = node.xpath(xpath)
-    except XPathEvalError:
-        parser_info['parser_warnings'].append('There was a XpathEvalError on the xpath: {} \n'
-            'Either it does not exist, or something is wrong with the expression.'.format(xpath))
-        # TODO maybe raise an error again to catch in upper routine, to know where exactly
-        return []
-    if len(return_value) == 1:
-        return return_value[0]
-    else:
-        return return_value
-'''
+
+# TODO should be used from somewhere else, probable double
 def convert_to_float(value_string, parser_info={'parser_warnings':[]}):
     """
     Tries to make a float out of a string. If it can't it logs a warning
@@ -332,27 +325,6 @@ def convert_to_float(value_string, parser_info={'parser_warnings':[]}):
         parser_info['parser_warnings'].append('Could not convert: "{}" to float, ValueError'.format(value_string))
         return value_string, False
     return value, True
-'''
-def get_xml_attribute(node, attributename):
-    """
-    Get an attribute value from a node.
-
-    :param node: a node from etree
-    :param attributename: a string with the attribute name.
-    :returns either attributevalue, or None
-    """
-    if etree.iselement(node):
-        attrib_value = node.get(attributename)
-        if attrib_value:
-            return attrib_value
-        else:
-            parser_info['parser_warnings'].append('Tried to get attribute: "{}" from element {}.\n '
-                   'I recieved "{}", maybe the attribute does not exist'.format(attributename, node, attrib_value))
-            return None
-    else: # something doesn't work here some nodes get through here
-        parser_info['parser_warnings'].append('Can not get attributename: "{}" from node "{}", because node is not an element of etree.'.format(attributename,node))
-        return None
-'''
 
 '''
 ### call
@@ -373,11 +345,13 @@ for calc in calcs_pks:
 
 print("--- %s seconds ---" % (time.time() - start_time))
 '''
+
+
 # TODO this is prob doubled is also in init_cls wc
-def clshifts_to_be(coreleveldict, reference_dict):
+def clshifts_to_be(coreleveldict, reference_dict, warn=False):
     """
     This methods converts corelevel shifts to binding energies, if a reference is given.
-    These cann than be used for plotting.
+    These can than be used for plotting.
 
     i.e
 
@@ -386,14 +360,31 @@ def clshifts_to_be(coreleveldict, reference_dict):
               'Be' : {'1s': [117]}}
     corelevels = {'W' : {'4f7/2' : [0.4, 0.3, 0.4 ,0.1],
                      '4f5/2' : [0, 0.3, 0.4, 0.1]},
-              'Be' : {'1s': [0, 0.2, 0.4, 0.1, 0.3]}}
+              'Be' : {'1s': [0, 0.2, 0.4, 0.1, 0.3]}
+              
+    {'Be': {'1s': [117, 117.2, 117.4, 117.1, 117.3]},
+     'W': {'4f5/2': [102, 102.3, 102.4, 102.1],
+           '4f7/2': [124.4, 124.3, 124.4, 124.1]}}
     """
     return_corelevel_dict = {}
+    
     for elem, corelevel_dict in coreleveldict.iteritems():
         ref_el = reference_dict.get(elem, {})
-        return_corelevel_dict[elem] = {}
+        
+        if not ref_el: # no refernce for that element given
+            if warn:
+                print("Warning: Reference for element: '{}' not given. "
+                      "I ignore these.".format(elem))
+            continue
+        
+        return_corelevel_dict[elem] = {}        
         for corelevel_name, corelevel_list in corelevel_dict.iteritems():
             ref_cl = ref_el.get(corelevel_name, [])
+            if not ref_cl: # no reference corelevel given for that element
+                if warn:
+                   print("Warning: Reference corelevel '{}' for element: '{}' "
+                         "not given. I ignore these.".format(corelevel_name, elem))                
+                continue
             be_all = []
             nref = len(ref_cl)
             ncl = len(corelevel_list)
@@ -409,20 +400,3 @@ def clshifts_to_be(coreleveldict, reference_dict):
 
 
     return return_corelevel_dict
-
-
-# test
-'''
-reference = {'W' : {'4f7/2' : [124],
-                     '4f5/2' : [102]},
-              'Be' : {'1s': [117]}}
-corelevels = {'W' : {'4f7/2' : [0.4, 0.3, 0.4 ,0.1],
-                     '4f5/2' : [0, 0.3, 0.4, 0.1]},
-              'Be' : {'1s': [0, 0.2, 0.4, 0.1, 0.3]}}
-clshifts_to_be(corelevels, reference)
-
-{'Be': {'1s': [117, 117.2, 117.4, 117.1, 117.3]},
- 'W': {'4f5/2': [102, 102.3, 102.4, 102.1],
-  '4f7/2': [124.4, 124.3, 124.4, 124.1]}}
-'''
-
