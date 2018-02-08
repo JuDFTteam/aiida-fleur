2d00876138c9cf877daa57fd2aad516bd5ff2050
broeder <j.broeder@fz-juelich.de>
2018-02-08 13:54:09 +0100

    added plot fleur method from plot methods to aiida-fleur package....

---
+++ aiida_fleur/tools/plot/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+FLEUR plug-in
+'''
+
+__copyright__ = u"Copyright (c), 2016-2017, Forschungszentrum Juelich, Germany. All rights reserved."
+__license__ = "MIT license, see LICENSE.txt file."
+__contributors__ = "Jens Broeder"
+__paper__ = ""
+__paper_short__ = ""
+
