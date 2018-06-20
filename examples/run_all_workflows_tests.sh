e1b52b51734a66081c04a4bb3383adb69f0c992f
broeder <j.broeder@fz-juelich.de>
2018-06-20 15:20:46 +0200

    resorted outside folder tests into examples and delete some things that are now covered by the integration tests

---
+++ examples/run_all_workflows_tests.sh
@@ -0,0 +1,5 @@
+# Run or submit all workflow tests
+# parse as argument the fleur and inpgen code you want to use, optional options, if non default for the used machine, 
+# if a number is parsed only that run test is launched. --submit will submit all
+
+# the materials and all other parameter are definded in the submission tests...
