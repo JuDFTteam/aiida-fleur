4154486f19da6eb06ecdebf66c33fdfdef546310
Vasily Tseplyaev <v.tseplyaev@fz-juelich.de>
2019-12-13 13:57:43 +0100

    Update test fixtures according to QE

--- aiida_fleur/tests/data/test_fleurinp.py
+++ aiida_fleur/tests/data/test_fleurinp.py
@@ -11,13 +11,12 @@
 ###############################################################################
 
 from __future__ import absolute_import
-import pytest
 import os
+import pytest
 
 ## Collect the input files
 file_path1 = '../files/inpxml/'
 file_path2 = '../files/nonvalid_inpxml'
-
 
 inpxmlfilefolder = os.path.dirname(os.path.abspath(__file__))
 inpxmlfilefolder_valid =  os.path.abspath(os.path.join(inpxmlfilefolder, file_path1))
@@ -34,66 +33,47 @@ for subdir, dirs, files in os.walk(inpxmlfilefolder_non_valid):
     for file in files:
         if file.endswith('.xml'):
             inpxmlfilelist2.append(os.path.join(subdir, file))
-##
 
 ### Tests
 
-# Testing of the Fleurinp data type
-
-
-# Test if valid fleurinp datas can be created for all inp.xml files in a given test folder
-        
 @pytest.mark.parametrize("inpxmlfilepath", inpxmlfilelist)
-@pytest.mark.usefixtures("aiida_env")
-def test_fleurinp_valid_inpxml(inpxmlfilepath, aiida_env):
+def test_fleurinp_valid_inpxml(aiida_profile, create_fleurinp, inpxmlfilepath):
     """
-    test if inp.xml file is reconnized as valid by fleur
+    test if valid inp.xml files are recognized by fleur
     """
-    from aiida.plugins import DataFactory
-    
-    fleurinp = DataFactory('fleur.fleurinp')
-    fleurinp_tmp = fleurinp(files=[inpxmlfilepath])
-    
-    assert fleurinp_tmp._has_schema == True
-    assert fleurinp._schema_file_path is not None
-    assert fleurinp_tmp.inp_dict != {}
-    fleurinp_tmp._validate # will throw validation error if not validating
+    fleurinp_tmp = create_fleurinp(inpxmlfilepath)
 
-    
+    assert fleurinp_tmp._has_schema
+    assert fleurinp_tmp._schema_file_path is not None
+    assert fleurinp_tmp.inp_dict != {}
+    assert fleurinp_tmp._validate is None # if fails, _validate throws an error
+
+
 @pytest.mark.parametrize("inpxmlfilepath", inpxmlfilelist2)
-@pytest.mark.usefixtures("aiida_env")
-def test_fleurinp_non_valid_inpxml(inpxmlfilepath):
+def test_fleurinp_non_valid_inpxml(aiida_profile, create_fleurinp, inpxmlfilepath):
     """
-    test what happens if inp.xml file is not valid
-    either an inputvalidationError should be thrown by fleurinpData or an 
+    Test what happens if inp.xml file is not valid
+    either an InputValidationError should be thrown by fleurinpData or an
     XMLSyntaxError error if the inp.xml does not correspond to the xml schema.
     """
     from aiida.common.exceptions import InputValidationError
     from lxml.etree import XMLSyntaxError
-    from aiida.plugins import DataFactory
-    
-    fleurinp = DataFactory('fleur.fleurinp')
-    
-    with pytest.raises((InputValidationError, XMLSyntaxError)) as error:
-        fleurinp_tmp = fleurinp(files=[inpxmlfilepath])
 
-        
+    with pytest.raises((InputValidationError, XMLSyntaxError)):
+        create_fleurinp(inpxmlfilepath)
+
+
 # test kpoints and structure and parameter data extraction
 
 @pytest.mark.parametrize("inpxmlfilepath", inpxmlfilelist)
-@pytest.mark.usefixtures("aiida_env")
-def test_fleurinp_kpointsdata_extraction(inpxmlfilepath):
+def test_fleurinp_kpointsdata_extraction(aiida_profile, create_fleurinp, inpxmlfilepath):
     """
     Extract a kpointsData from the fleurinp data, i.e inp.xml and check if 
     the resulting node is a valid kpoints data
     """
-    from aiida.plugins import DataFactory
+    from aiida.orm import KpointsData
     
-    KpointsData = DataFactory('array.kpoints')
-    fleurinp = DataFactory('fleur.fleurinp')
-
-    
-    fleurinp_tmp = fleurinp(files=[inpxmlfilepath])
+    fleurinp_tmp = create_fleurinp(inpxmlfilepath)
     kptsd = fleurinp_tmp.get_kpointsdata_ncf()
     
     if kptsd is not None:
@@ -105,38 +85,30 @@ def test_fleurinp_kpointsdata_extraction(inpxmlfilepath):
 
 
 @pytest.mark.parametrize("inpxmlfilepath", inpxmlfilelist)
-@pytest.mark.usefixtures("aiida_env")
-def test_fleurinp_parameterdata_extraction(inpxmlfilepath):
+def test_fleurinp_parameterdata_extraction(aiida_profile, create_fleurinp, inpxmlfilepath):
     """
-    Extract a ParameterData from the fleurinp data, i.e inp.xml and check if 
-    the resulting node is a valid ParameterData. ggf if it can be used by inpgen  
+    Extract a ParameterData from the fleurinp data, i.e inp.xml and check if
+    the resulting node is a valid ParameterData. ggf if it can be used by inpgen
     """
-    from aiida.plugins import DataFactory
-    
-    ParameterData = DataFactory('dict')
-    fleurinp = DataFactory('fleur.fleurinp')
-    
-    fleurinp_tmp = fleurinp(files=[inpxmlfilepath])
+    from aiida.orm import Dict
+
+    fleurinp_tmp = create_fleurinp(inpxmlfilepath)
     param = fleurinp_tmp.get_parameterdata_ncf()
-    
-    assert isinstance(param, ParameterData)
-    
+
+    assert isinstance(param, Dict)
+
     # ToDo check if it is also right for inpgen...
 
 
 @pytest.mark.parametrize("inpxmlfilepath", inpxmlfilelist)
-@pytest.mark.usefixtures("aiida_env")
-def test_fleurinp_structuredata_extraction(inpxmlfilepath):
+def test_fleurinp_structuredata_extraction(aiida_profile, create_fleurinp, inpxmlfilepath):
     """
-    Extract a ParameterData from the fleurinp data, i.e inp.xml and check if 
+    Extract a ParameterData from the fleurinp data, i.e inp.xml and check if
     the resulting node is a valid ParameterData.
     """
-    from aiida.plugins import DataFactory
-    
-    StructureData = DataFactory('structure')
-    fleurinp = DataFactory('fleur.fleurinp')
-    
-    fleurinp_tmp = fleurinp(files=[inpxmlfilepath])
+    from aiida.orm import StructureData
+
+    fleurinp_tmp = create_fleurinp(inpxmlfilepath)
     struc = fleurinp_tmp.get_structuredata_ncf()
 
     if struc is not None:
@@ -151,36 +123,27 @@ def test_fleurinp_structuredata_extraction(inpxmlfilepath):
        
 # Input Modification tests
 @pytest.mark.parametrize("inpxmlfilepath", inpxmlfilelist)
-@pytest.mark.usefixtures("aiida_env")
-def test_fleurinp_single_value_modification(inpxmlfilepath):
+def test_fleurinp_single_value_modification(aiida_profile, create_fleurinp, inpxmlfilepath):
     """
     set kmax, itmax, minDistance in inp.xml input file of fleurinpdata to 
     10.2, 99, 0.000001, then check if it everything set
     """
-    from aiida.plugins import DataFactory
     from aiida_fleur.data.fleurinpmodifier import FleurinpModifier
 
-    
-    fleurinp = DataFactory('fleur.fleurinp')
-    
-    fleurinp_tmp = fleurinp(files=[inpxmlfilepath])
+    fleurinp_tmp = create_fleurinp(inpxmlfilepath)
     fleurinpmode = FleurinpModifier(fleurinp_tmp)
-    # caps matters here!
+
     fleurinpmode.set_inpchanges({'itmax': 99, 'minDistance' : 0.01, 'Kmax' : 10.2})
-    
-    
+
     fleurinpmode.show(display=False, validate=True)
     out = fleurinpmode.freeze()
-    
-    assert isinstance(out, fleurinp)
-    
+
+    assert isinstance(out, type(fleurinp_tmp))
     # TODO check if set right
     
 @pytest.mark.parametrize("inpxmlfilepath", inpxmlfilelist)
-@pytest.mark.usefixtures("aiida_env")
-def test_fleurinp_first_species_modification(inpxmlfilepath):
+def test_fleurinp_first_species_modification(aiida_profile, create_fleurinp, inpxmlfilepath):
     """
     Decrease the rmt of the first species by 10%, check if rmt was set
     """
     pass
-
--- aiida_fleur/tests/test_entrypoints.py
+++ aiida_fleur/tests/test_entrypoints.py
@@ -13,54 +13,47 @@
 from __future__ import absolute_import
 import pytest
 
-@pytest.mark.usefixtures("aiida_env")
-class TestAiida_fleur_entrypoints:
+@pytest.mark.usefixtures("aiida_profile")
+class TestFleurEntrypoints:
     """
-    tests all the entry points of the Fleur plugin. Therefore if the plugin is 
-    reconized by AiiDA and installed right. 
+    tests all the entry points of the Fleur plugin. Therefore if the plugin is
+    recognized by AiiDA and installed right.
     """
-    
+
     # Calculations
-    
-    def test_inpgen_calculation_entry_point(aiida_env):
+
+    def test_inpgen_calculation_entry_point(self):
         from aiida.plugins import CalculationFactory
         inpgen_calculation = CalculationFactory('fleur.inpgen')
         assert inpgen_calculation is not None
 
-    def test_fleur_calculation_entry_point(aiida_env):
+    def test_fleur_calculation_entry_point(self):
         from aiida.plugins import CalculationFactory
-        
+
         fleur_calculation = CalculationFactory('fleur.fleur')
         assert fleur_calculation is not None
-    
-    
+
+
     # Data
-    
-    def test_fleur_fleurinpdata_entry_point(aiida_env):
+
+    def test_fleur_fleurinpdata_entry_point(self):
         from aiida.plugins import DataFactory
         from aiida_fleur.data.fleurinp import FleurinpData
     
         fleurinp = DataFactory('fleur.fleurinp')
         assert fleurinp == FleurinpData
 
-    def test_fleur_fleurinpmodifier_entry_point(aiida_env):
-        from aiida.plugins import DataFactory
-        from aiida_fleur.data.fleurinpmodifier import FleurinpModifier
-
-        fleurinpmod = DataFactory('fleur.fleurinpmodifier')
-        assert fleurinpmod == FleurinpModifier
-
 
     # Parsers
 
-    def test_inpgen_parser_entry_point(aiida_env):
+    def test_inpgen_parser_entry_point(self):
         from aiida.plugins import ParserFactory
         from aiida_fleur.parsers.fleur_inputgen import Fleur_inputgenParser
 
         parser = ParserFactory('fleur.fleurinpgenparser')
         assert parser == Fleur_inputgenParser
 
-    def test_fleur_parser_entry_point(aiida_env):
+    def test_fleur_parser_entry_point(self):
         from aiida.plugins import ParserFactory
         from aiida_fleur.parsers.fleur import FleurParser
 
@@ -70,101 +63,101 @@ class TestAiida_fleur_entrypoints:
 
     # Workflows/workchains
 
-    def test_fleur_scf_wc_entry_point(aiida_env):
+    def test_fleur_scf_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.scf import FleurScfWorkChain
-        
+
         workflow = WorkflowFactory('fleur.scf')
         assert workflow == FleurScfWorkChain
-        
-    def test_fleur_dos_wc_entry_point(aiida_env):
+
+    def test_fleur_dos_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.dos import fleur_dos_wc
-        
+
         workflow = WorkflowFactory('fleur.dos')
         assert workflow == fleur_dos_wc
-        
-    def test_fleur_band_wc_entry_point(aiida_env):
+
+    def test_fleur_band_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.band import fleur_band_wc
-        
+
         workflow = WorkflowFactory('fleur.band')
         assert workflow == fleur_band_wc
 
-    def test_fleur_eos_wc_entry_point(aiida_env):
+    def test_fleur_eos_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.eos import FleurEosWorkChain
-        
+
         workflow = WorkflowFactory('fleur.eos')
         assert workflow == FleurEosWorkChain
-    
+
     # this entry point has currently a problem...
-    def test_fleur_inital_cls_wc_entry_point(aiida_env):
+    def test_fleur_initial_cls_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.initial_cls import fleur_initial_cls_wc
 
         workflow = WorkflowFactory('fleur.init_cls')
         assert workflow == fleur_initial_cls_wc
-        
-    def test_fleur_corehole_wc_entry_point(aiida_env):
+
+    def test_fleur_corehole_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.corehole import fleur_corehole_wc
-        
+
         workflow = WorkflowFactory('fleur.corehole')
         assert workflow == fleur_corehole_wc
 
-    def test_fleur_mae_wc_entry_point(aiida_env):
+    def test_fleur_mae_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.mae import FleurMaeWorkChain
-        
+
         workflow = WorkflowFactory('fleur.mae')
         assert workflow == FleurMaeWorkChain
 
-    def test_fleur_mae_conv_wc_entry_point(aiida_env):
+    def test_fleur_mae_conv_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.mae_conv import FleurMaeConvWorkChain
-        
+
         workflow = WorkflowFactory('fleur.mae_conv')
         assert workflow == FleurMaeConvWorkChain
 
-    def test_fleur_ssdisp_wc_entry_point(aiida_env):
+    def test_fleur_ssdisp_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.ssdisp import FleurSSDispWorkChain
-        
+
         workflow = WorkflowFactory('fleur.ssdisp')
         assert workflow == FleurSSDispWorkChain
-    
-    def test_fleur_ssdisp_wc_entry_point(aiida_env):
+
+    def test_fleur_ssdisp_conv_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.ssdisp_conv import FleurSSDispConvWorkChain
-        
+
         workflow = WorkflowFactory('fleur.ssdisp_conv')
         assert workflow == FleurSSDispConvWorkChain
 
-    def test_fleur_dmi_wc_entry_point(aiida_env):
+    def test_fleur_dmi_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.dmi import FleurDMIWorkChain
-        
+
         workflow = WorkflowFactory('fleur.dmi')
         assert workflow == FleurDMIWorkChain
-   
-    def test_fleur_base_wc_entry_point(aiida_env):
+
+    def test_fleur_base_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.base_fleur import FleurBaseWorkChain
-        
+
         workflow = WorkflowFactory('fleur.base')
         assert workflow == FleurBaseWorkChain
 
-    def test_fleur_base_relax_wc_entry_point(aiida_env):
+    def test_fleur_base_relax_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.base_relax import FleurBaseRelaxWorkChain
-        
+
         workflow = WorkflowFactory('fleur.base_relax')
         assert workflow == FleurBaseRelaxWorkChain
 
-    def test_fleur_create_magnetic_wc_entry_point(aiida_env):
+    def test_fleur_create_magnetic_wc_entry_point(self):
         from aiida.plugins import WorkflowFactory
         from aiida_fleur.workflows.create_magnetic_film import FleurCreateMagneticWorkChain
-        
+
         workflow = WorkflowFactory('fleur.create_magnetic')
         assert workflow == FleurCreateMagneticWorkChain
