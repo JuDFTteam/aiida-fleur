b6ff5717aaab30b47f77164d070461a9fe4d1e05
Jens Bröder <j.broeder@fz-juelich.de>
2020-01-31 10:42:17 +0100

    added method in tools/io_routines to delete eigenvalues and iterations from out.xml files, can be used for data compression. also wrote a test for this

--- aiida_fleur/tools/io_routines.py
+++ aiida_fleur/tools/io_routines.py
@@ -11,7 +11,7 @@
 ###############################################################################
 
 """
-Here we collect io routines and their utility. For writting certain things to files.
+Here we collect IO routines and their utility, for writting certain things to files, or post process files.
 For example collection of data or database evaluations, for other people.
 """
 
@@ -91,3 +91,85 @@ def write_xps_spectra_datafile(nodes, factors, natomtypes_dict, bindingenergies,
 #factors = [1,1]
 #returnvalues = plot_spectra(all_wc_BeTi_uuid, factors=factors, energy_range=[109,112], energy_grid=0.2)
 #write_xps_spectra_datafile(all_wc_BeTi_uuid, factors, *returnvalues, destination='./out.txt')
+
+
+def compress_fleuroutxml(outxmlfilepath, dest_file_path=None, delete_eig=True, iterations_to_keep=None):
+    """
+    Compresses a fleur out.xml file by deleting certain things 
+    like eigenvalues tags and/or iterations from it
+    
+    :param outxmlfilepath: (absolut) file path
+    :type outxmlfilepath: str
+    :param dest_file_path: (absolut) for the compressed file to be saved, if no desitination file path is given the file is overriden in place (default)!
+    :type dest_file_path: str, optional
+    :param delete_eig:  if eigenvalues are deleted from file default is True
+    :type delete_eig: boolean, optional 
+    :param iterations_to_keep: index of 'till' whihc iterations to be keep, i.e '-2' means only last two, '15' default (None) is keep all 
+    :type iterations_to_keep: int
+    
+     ###
+     usage example:
+     outxmldes = '/Users/broeder/test/FePt_out_test.xml'
+     outxmlsrc = '/Users/broeder/test/FePt_out.xml'
+     compress_fleuroutxml(outxmlsrc, dest_file_path=outxmldes, iterations_to_keep=14)
+     compress_fleuroutxml(outxmlsrc, dest_file_path=outxmldes, iterations_to_keep=-1)
+
+    
+    """
+    from aiida_fleur.tools.xml_util import delete_tag, eval_xpath2
+    from lxml import etree
+    
+    xpath_eig = '/fleurOutput/scfLoop/iteration/eigenvalues'
+    xpath_iter = '/fleurOutput/scfLoop/iteration'
+    
+    parser = etree.XMLParser(recover=False)
+    outfile_broken = False
+    try:
+        tree = etree.parse(outxmlfilepath, parser)
+    except etree.XMLSyntaxError:
+        outfile_broken = True
+        print('broken')
+        
+    if outfile_broken:
+        # repair xmlfile and try to parse what is possible.
+        parser = etree.XMLParser(recover=True)
+        try:
+            tree = etree.parse(outxmlfile, parser)
+        except etree.XMLSyntaxError:
+            parse_xml = False
+            successful = False
+    
+    # delete eigenvalues (all) 
+    if delete_eig:
+        new_etree = delete_tag(tree, xpath_eig)
+    
+    # delete certain iterations
+    if iterations_to_keep is not None:
+        root = new_etree.getroot()
+        iteration_nodes = eval_xpath2(root, xpath_iter)
+        n_iters = len(iteration_nodes)
+        print(n_iters)
+        if iterations_to_keep < 0:
+            # the first element has 1 (not 0) in xpath expresions
+            position_keep = n_iters + iterations_to_keep + 1
+            delete_xpath = xpath_iter +"[position()<{}]".format(int(position_keep))
+        else:
+            delete_xpath = xpath_iter +"[position()>{}]".format(int(iterations_to_keep))
+
+        if abs(iterations_to_keep) > n_iters:
+            print('Warning: iterations_to_keep is larger then the number of iterations'
+                  ' in the given out.xml file, I keep all.')
+        else:
+            print(delete_xpath)
+            new_etree = delete_tag(new_etree, delete_xpath)
+        
+    if dest_file_path is None:
+        dest_file_path = outxmlfilepath       # overwrite file
+    new_etree.write(dest_file_path)
+    
+    return
+# usage example
+#outxml15 = '/Users/broeder/test/FePt_out_test.xml'
+#outxml14 = '/Users/broeder/test/FePt_out.xml'
+#compress_fleuroutxml(outxml14, dest_file_path=outxml15, iterations_to_keep=14)
+#compress_fleuroutxml(outxml14, dest_file_path=outxml15, iterations_to_keep=-1)
