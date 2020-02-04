f7dc44bcf91a6d7cb270d2c1b21cbe4927508525
Vasily Tseplyaev <v.tseplyaev@fz-juelich.de>
2020-02-04 11:06:27 +0100

    Small code cleanup

--- examples/submission/workflow_tests/test_submit_create_magnetic.py
+++ examples/submission/workflow_tests/test_submit_create_magnetic.py
@@ -49,7 +49,6 @@ args = parser.parse_args()
 print(args)
 
 
-
 fleur_code = is_code(args.fleur)
 fleur_code = test_and_get_codenode(fleur_code, expected_code_type='fleur.fleur')
 
@@ -59,36 +58,36 @@ if args.inpgen is not None:
 
 ####
 wf_para = {
-        'lattice': 'fcc',
-        'miller': [[-1, 1, 0],
-                   [0, 0, 1],
-                   [1, 1, 0]],
-        'host_symbol': 'Pt',
-        'latticeconstant': 4.0,
-        'size': (1, 1, 5),
-        'replacements': {0: 'Fe', -1: 'Fe'},
-        'decimals': 10,
-        'pop_last_layers': 1,
+    'lattice': 'fcc',
+    'miller': [[-1, 1, 0],
+               [0, 0, 1],
+               [1, 1, 0]],
+    'host_symbol': 'Pt',
+    'latticeconstant': 4.0,
+    'size': (1, 1, 5),
+    'replacements': {0: 'Fe', -1: 'Fe'},
+    'decimals': 10,
+    'pop_last_layers': 1,
 
-        'total_number_layers': 8,
-        'num_relaxed_layers': 3
-    }
+    'total_number_layers': 8,
+    'num_relaxed_layers': 3
+}
 
 wf_para = Dict(dict=wf_para)
 
 wf_eos = {
-        'points': 15,
-        'step': 0.015,
-        'guess': 1.00
-        }
+    'points': 15,
+    'step': 0.015,
+    'guess': 1.00
+}
 
 wf_eos_scf = {
-        'fleur_runmax': 4,
-        'density_converged': 0.0002,
-        'serial': False,
-        'itmax_per_run': 50,
-        'inpxml_changes': []
-        }
+    'fleur_runmax': 4,
+    'density_converged': 0.0002,
+    'serial': False,
+    'itmax_per_run': 50,
+    'inpxml_changes': []
+}
 
 wf_eos_scf = Dict(dict=wf_eos_scf)
 
@@ -97,42 +96,42 @@ wf_eos = Dict(dict=wf_eos)
 calc_eos = {
     'comp': {
         'kmax': 3.8,
-        },
+    },
     'kpt': {
         'div1': 4,
-        'div2' : 4,
-        'div3' : 4
-        }
+        'div2': 4,
+        'div3': 4
+    }
 }
 
 calc_eos = Dict(dict=calc_eos)
 
-options_eos = {'resources' : {"num_machines": 1, "num_mpiprocs_per_machine" : 4, "num_cores_per_mpiproc" : 6},
-               'queue_name' : 'devel',
-               'environment_variables' : {'OMP_NUM_THREADS' : '6'},
-               'custom_scheduler_commands' : '',
+options_eos = {'resources': {"num_machines": 1, "num_mpiprocs_per_machine": 4, "num_cores_per_mpiproc": 6},
+               'queue_name': 'devel',
+               'environment_variables': {'OMP_NUM_THREADS': '6'},
+               'custom_scheduler_commands': '',
                'max_wallclock_seconds':  1*60*60}
 
 options_eos = Dict(dict=options_eos)
 
 wf_relax = {
-        'film_distance_relaxation' : False,
-        'force_criterion': 0.049,
-        'use_relax_xml': True
-    }
+    'film_distance_relaxation': False,
+    'force_criterion': 0.049,
+    'use_relax_xml': True
+}
 
 wf_relax_scf = {
-        'fleur_runmax': 5,
-        'serial': False,
-        'itmax_per_run': 50,
-        'alpha_mix': 0.015,
-        'relax_iter': 25,
-        'force_converged': 0.001,
-        'force_dict': {'qfix': 2,
-                       'forcealpha': 0.75,
-                       'forcemix': 'straight'},
-        'inpxml_changes': []
-        }
+    'fleur_runmax': 5,
+    'serial': False,
+    'itmax_per_run': 50,
+    'alpha_mix': 0.015,
+    'relax_iter': 25,
+    'force_converged': 0.001,
+    'force_dict': {'qfix': 2,
+                   'forcealpha': 0.75,
+                   'forcemix': 'straight'},
+    'inpxml_changes': []
+}
 
 wf_relax = Dict(dict=wf_relax)
 wf_relax_scf = Dict(dict=wf_relax_scf)
@@ -140,35 +139,35 @@ wf_relax_scf = Dict(dict=wf_relax_scf)
 calc_relax = {
     'comp': {
         'kmax': 4.0,
-        },
+    },
     'kpt': {
         'div1': 24,
-        'div2' : 20,
-        'div3' : 1
-        },
-    'atom':{
-        'element' : 'Pt',
-        'rmt' : 2.2,
-        'lmax' : 10,
-        'lnonsph' : 6,
+        'div2': 20,
+        'div3': 1
+    },
+    'atom': {
+        'element': 'Pt',
+        'rmt': 2.2,
+        'lmax': 10,
+        'lnonsph': 6,
         'econfig': '[Kr] 5s2 4d10 4f14 5p6| 5d9 6s1',
-        },
-    'atom2':{
-        'element' : 'Fe',
-        'rmt' : 2.1,
-        'lmax' : 10,
-        'lnonsph' : 6,
+    },
+    'atom2': {
+        'element': 'Fe',
+        'rmt': 2.1,
+        'lmax': 10,
+        'lnonsph': 6,
         'econfig': '[Ne] 3s2 3p6| 3d6 4s2',
-        },
+    },
 }
 
 calc_relax = Dict(dict=calc_relax)
 
-options_relax = {'resources' : {"num_machines": 1, "num_mpiprocs_per_machine" : 4, "num_cores_per_mpiproc" : 6},
-                     'queue_name' : 'devel',
-                     'environment_variables' : {'OMP_NUM_THREADS' : '6'},
-                     'custom_scheduler_commands' : '',
-                     'max_wallclock_seconds':  1*60*60}
+options_relax = {'resources': {"num_machines": 1, "num_mpiprocs_per_machine": 4, "num_cores_per_mpiproc": 6},
+                 'queue_name': 'devel',
+                 'environment_variables': {'OMP_NUM_THREADS': '6'},
+                 'custom_scheduler_commands': '',
+                 'max_wallclock_seconds':  1*60*60}
 
 options_relax = Dict(dict=options_relax)
 
@@ -183,29 +182,29 @@ inpgen_inp = test_and_get_codenode(inpgen_code, expected_code_type='fleur.inpgen
 inputs = {
     'eos': {
         'scf': {
-                  'wf_parameters' : wf_eos_scf,
-                  'calc_parameters' : calc_eos,
-                  'options' : options_eos,
-                  'inpgen' : inpgen_inp,
-                  'fleur' : fleur_inp
+            'wf_parameters': wf_eos_scf,
+            'calc_parameters': calc_eos,
+            'options': options_eos,
+            'inpgen': inpgen_inp,
+            'fleur': fleur_inp
         },
-        'wf_parameters' : wf_eos
+        'wf_parameters': wf_eos
     },
     'relax': {
-        'scf' : {
-                  'wf_parameters' : wf_relax_scf,
-                  'calc_parameters' : calc_relax,
-                  'options' : options_relax,
-                  'inpgen' : inpgen_inp,
-                  'fleur' : fleur_inp
+        'scf': {
+            'wf_parameters': wf_relax_scf,
+            'calc_parameters': calc_relax,
+            'options': options_relax,
+            'inpgen': inpgen_inp,
+            'fleur': fleur_inp
         },
-        'wf_parameters' : wf_relax,
+        'wf_parameters': wf_relax,
         'label': 'relaxation',
-        'description' : 'describtion',
-        'max_iterations' : Int(5)
+        'description': 'describtion',
+        'max_iterations': Int(5)
     },
     'wf_parameters': wf_para
-    #'eos_output': load_node(14405)
+    # 'eos_output': load_node(14405)
 }
 
 
