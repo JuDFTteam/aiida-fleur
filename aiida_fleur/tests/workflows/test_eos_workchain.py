cf694635d9104b2f02fd9afd9d1962e24c048bd4
Jens Bröder <j.broeder@fz-juelich.de>
2020-03-21 13:33:54 +0100

    added full regression test for eos workchain, works if only the file is tested, fails if all tests are run...

--- aiida_fleur/tests/workflows/test_eos_workchain.py
+++ aiida_fleur/tests/workflows/test_eos_workchain.py
@@ -28,13 +28,12 @@ CALC2_ENTRY_POINT = 'fleur.inpgen'
 
 # tests
 @pytest.mark.usefixtures("aiida_profile", "clear_database")
-class Test_FleurEosWorkChain():
+class Test_FleurEosWorkChain:
     """
     Regression tests for the FleurEosWorkChain
     """
-    @pytest.mark.skip(reason="Test is not implemented")
     @pytest.mark.timeout(500, method='thread')
-    def test_fleur_eos_structure_Si(self, run_with_cache, mock_code_factory, generate_structure):
+    def test_fleur_eos_structure_Si(self, run_with_cache, fleur_local_code, inpgen_local_code, generate_structure2):
         """
         full example using scf workflow with just a fleurinp data as input.
         Several fleur runs needed till convergence
@@ -46,61 +45,102 @@ class Test_FleurEosWorkChain():
                 "num_machines": 1,
                 "num_mpiprocs_per_machine": 1
             },
-            'max_wallclock_seconds': 5 * 60,
+            'max_wallclock_seconds': 10 * 60,
             'withmpi': False,
             'custom_scheduler_commands': ''
         }
+        wf_param = {'points': 7, 'step': 0.002, 'guess': 1.00}
 
-        FleurCode = mock_code = mock_code_factory(
-            label='fleur',
-            data_dir_abspath=os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), 'calc_data_dir/'
-            ),
-            entry_point=CALC_ENTRY_POINT,
-            ignore_files=[
-                '_aiidasubmit.sh', 'cdnc', 'out', 'FleurInputSchema.xsd', 'cdn.hdf', 'usage.json',
-                'cdn??'
-            ]
-        )
+        calc_parameters = {
+            'atom': {
+                'element': "Si",
+                'rmt': 2.1,
+                'jri': 981,
+                'lmax': 8,
+                'lnonsph': 6
+            },
+            'comp': {
+                'kmax': 3.4
+            },
+            'kpt': {
+                'div1': 10,
+                'div2': 10,
+                'div3': 10,
+                'tkb': 0.0005
+            }
+        }
+
+        # Fe fcc structure
+        bohr_a_0 = 0.52917721092  # A
+        a = 3.4100000000 * 2**(0.5)
+        cell = [[a, 0, 0], [0, a, 0], [0, 0, a]]
+        structure = StructureData(cell=cell)
+        structure.append_atom(position=(0., 0., 0.), symbols='Fe', name='Fe1')
+        structure.append_atom(position=(0.5 * a, 0.5 * a, 0.0 * a), symbols='Fe', name='Fe2')
+        structure.append_atom(position=(0.5 * a, 0.0 * a, 0.5 * a), symbols='Fe', name='Fe31')
+        structure.append_atom(position=(0.0 * a, 0.5 * a, 0.5 * a), symbols='Fe', name='Fe43')
+        calc_parameters = {
+                'comp': {
+                    'kmax': 3.4,
+                },
+                'atom': {
+                    'element': 'Fe',
+                    'bmu': 2.5,
+                    'rmt': 2.15
+                },
+                'kpt': {
+                    'div1': 4,
+                    'div2': 4,
+                    'div3': 4
+                }
+            }
+
+
+        wf_para_scf = {
+            'fleur_runmax': 2,
+            'itmax_per_run': 120,
+            'density_converged': 0.2,
+            'serial': True,
+            'mode': 'density'
+        }
+
+
+        FleurCode = fleur_local_code
+        InpgenCode = inpgen_local_code
+
         # create process builder to set parameters
         builder = FleurEosWorkChain.get_builder()
-        builder.metadata.description = 'Simple Fleur FleurEosWorkChain test for Si bulk with fleurinp data given'
+        builder.metadata.description = 'Simple Fleur FleurEosWorkChain test for Si bulk'
         builder.metadata.label = 'FleurEosWorkChain_test_Si_bulk'
-        builder.structure = generate_structure().store()
-        builder.scf.options = Dict(dict=options)
-        builder.scf.fleur = FleurCode
-
+        builder.structure = structure.store()#generate_structure2().store()
+        builder.wf_parameters = Dict(dict=wf_param).store()
+        builder.scf = {'fleur' : FleurCode, 'inpgen': InpgenCode,
+                      'options' : Dict(dict=options).store(),
+                      'wf_parameters' : Dict(dict=wf_para_scf).store(),
+                      'calc_parameters' : Dict(dict=calc_parameters).store()}
+        print(builder)
         # now run calculation
-        run_with_cache(builder)
-        out, node = run_get_node(builder)
+        out, node = run_with_cache(builder)
+
         print(out)
         print(node)
+
         outpara = out.get('output_eos_wc_para', None)
         assert outpara != None
         outpara = outpara.get_dict()
         print(outpara)
 
+        outstruc = out.get('output_eos_wc_structure', None)
+        assert outstruc != None
+
+        assert node.is_finished_ok
+
         # check output
-        assert False
         #distance, bulk modulus, optimal structure, opt scaling
-        #assert abs(n.get('starting_fermi_energy') - 0.409241) < 10**-14
+        assert abs(outpara.get('scaling_gs') - 0.99268546558578) < 10**14
+        assert outpara.get('warnings') == ['Groundstate volume was not in the scaling range.']
+        assert outpara.get('info') == ['Consider rerunning around point 0.9926854655857787']
 
-    @pytest.mark.skip(reason="Test is not implemented")
-    @pytest.mark.timeout(500, method='thread')
-    def test_fleur_eos_structure_non_cubic(self, run_with_cache, mock_code_factory):
-        """
-        Full regression test of FleurEosWorkChain starting with a crystal structure and parameters
-        """
-        assert False
-
-    @pytest.mark.skip(reason="Test is not implemented")
-    @pytest.mark.timeout(500, method='thread')
-    def test_fleur_eos_one_simulation_fails(self, run_with_cache, mock_code_factory):
-        """
-        Full regression test of FleurEosWorkChain starting with a fleurinp data,
-        but adjusting the Fleur input file before the fleur run.
-        """
-        assert False
 
     @pytest.mark.timeout(500, method='thread')
     def test_fleur_eos_validation_wrong_inputs(
