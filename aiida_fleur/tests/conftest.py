da530c18c973365238de3bfeb6ec333d1767b14c
Jens Bröder <j.broeder@fz-juelich.de>
2020-04-03 17:21:41 +0200

    docs should build now, code line was in wrong place for creation of test codes

--- aiida_fleur/tests/conftest.py
+++ aiida_fleur/tests/conftest.py
@@ -439,14 +439,15 @@ def create_or_fake_local_code(aiida_local_code_factory):
         from aiida.tools.importexport import import_data, export
         from aiida.orm import ProcessNode, QueryBuilder, Code, load_node
 
-        # make sure code is found in PATH
         _exe_path = os.path.abspath(exec_relpath)
-        os.environ['PATH']+=':'+_exe_path
 
         # if path is non existent, we create a dummy executable
         # if all caches are there, it should run, like on a CI server
         if not os.path.exists(_exe_path):
             open(_exe_path, 'a').close()
+
+        # make sure code is found in PATH
+        os.environ['PATH']+=':'+_exe_path
 
         # get code using aiida_local_code_factory fixture
         code = aiida_local_code_factory(entrypoint, executable)
