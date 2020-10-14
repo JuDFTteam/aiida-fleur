f2231514877a26bc4db6687fbb08ff41d2b2a508
Vasily Tseplyaev <v.tseplyaev@fz-juelich.de>
2020-10-14 09:53:53 +0200

    Fix double quoted strings

--- docs/source/user_guide/workflows/code/initial_cls_wc_outputnode.py
+++ docs/source/user_guide/workflows/code/initial_cls_wc_outputnode.py
@@ -1,36 +1,36 @@
 # -*- coding: utf-8 -*-
 {
-    "atomtypes": {
-        "W": [
+    'atomtypes': {
+        'W': [
             {
-                "atomic_number": 74,
-                "coreconfig": "[Kr] (5s1/2) (4d3/2) (4d5/2) (4f5/2) (4f7/2)",
-                "element": "W",
-                "natoms": 1,
-                "species": "W-1",
-                "stateOccupation": [
+                'atomic_number': 74,
+                'coreconfig': '[Kr] (5s1/2) (4d3/2) (4d5/2) (4f5/2) (4f7/2)',
+                'element': 'W',
+                'natoms': 1,
+                'species': 'W-1',
+                'stateOccupation': [
                     {
-                        "(5d3/2)": [
-                            "2.00000000",
-                            ".00000000"
+                        '(5d3/2)': [
+                            '2.00000000',
+                            '.00000000'
                         ]
                     },
                     {
-                        "(5d5/2)": [
-                            "2.00000000",
-                            ".00000000"
+                        '(5d5/2)': [
+                            '2.00000000',
+                            '.00000000'
                         ]
                     }
                 ],
-                "valenceconfig": "(5p1/2) (5p3/2) (6s1/2) (5d3/2) (5d5/2)"
+                'valenceconfig': '(5p1/2) (5p3/2) (6s1/2) (5d3/2) (5d5/2)'
             }
         ]
     },
-    "bandgap": 0.0074571775,
-    "bandgap_units": "htr",
-    "binding_energy_convention": "negativ",
-    "corelevel_energies": {
-        "W": [
+    'bandgap': 0.0074571775,
+    'bandgap_units': 'htr',
+    'binding_energy_convention': 'negativ',
+    'corelevel_energies': {
+        'W': [
             [
                 -2550.2512204246,
                 -439.7260486989,
@@ -52,9 +52,9 @@
             ]
         ]
     },
-    "corelevel_energies_units": "htr",
-    "corelevelshifts": {
-        "W": [
+    'corelevel_energies_units': 'htr',
+    'corelevelshifts': {
+        'W': [
             [
                 0.0,
                 0.0,
@@ -63,22 +63,22 @@
             ]
         ]
     },
-    "corelevelshifts_units": "htr",
-    "fermi_energy": 0.6026436555,
-    "fermi_energy_units": "htr",
-    "formation_energy": [
+    'corelevelshifts_units': 'htr',
+    'fermi_energy': 0.6026436555,
+    'fermi_energy_units': 'htr',
+    'formation_energy': [
         0.0
     ],
-    "formation_energy_units": "eV/atom",
-    "material": "W",
-    "reference_bandgaps": [
+    'formation_energy_units': 'eV/atom',
+    'material': 'W',
+    'reference_bandgaps': [
         0.0074571775
     ],
-    "reference_bandgaps_des": [
-        "W"
+    'reference_bandgaps_des': [
+        'W'
     ],
-    "reference_corelevel_energies": {
-        "W": [
+    'reference_corelevel_energies': {
+        'W': [
             [
                 -2550.2512204246,
                 -439.7260486989,
@@ -100,23 +100,23 @@
             ]
         ]
     },
-    "reference_corelevel_energies_units": "htr",
-    "reference_fermi_energy": [
+    'reference_corelevel_energies_units': 'htr',
+    'reference_fermi_energy': [
         0.6026436555
     ],
-    "reference_fermi_energy_des": [
-        "W"
+    'reference_fermi_energy_des': [
+        'W'
     ],
-    "successful": true,
-    "total_energy": -439902.56049548,
-    "total_energy_ref": [
+    'successful': true,
+    'total_energy': -439902.56049548,
+    'total_energy_ref': [
         -439902.56049548
     ],
-    "total_energy_ref_des": [
-        "W"
+    'total_energy_ref_des': [
+        'W'
     ],
-    "total_energy_units": "eV",
-    "warnings": [],
-    "workflow_name": "fleur_initial_cls_wc",
-    "workflow_version": "0.4.0"
+    'total_energy_units': 'eV',
+    'warnings': [],
+    'workflow_name': 'fleur_initial_cls_wc',
+    'workflow_version': '0.4.0'
 }
