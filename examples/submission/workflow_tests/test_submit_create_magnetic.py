00fec381e5960eafed38538d637425330a23750d
Vasily Tseplyaev <v.tseplyaev@fz-juelich.de>
2020-10-18 15:50:39 +0200

    Revert "Remove OMP_NUM_THREADS env variable"
    This reverts commit 9176fcbf50dd9971eb7815177b071eb5f89ca02e.

--- examples/submission/workflow_tests/test_submit_create_magnetic.py
+++ examples/submission/workflow_tests/test_submit_create_magnetic.py
@@ -109,6 +109,9 @@ options_eos = {
         'num_cores_per_mpiproc': 6
     },
     'queue_name': 'devel',
+    'environment_variables': {
+        'OMP_NUM_THREADS': '6'
+    },
     'custom_scheduler_commands': '',
     'max_wallclock_seconds': 1 * 60 * 60
 }
@@ -169,6 +172,9 @@ options_relax = {
         'num_cores_per_mpiproc': 6
     },
     'queue_name': 'devel',
+    'environment_variables': {
+        'OMP_NUM_THREADS': '6'
+    },
     'custom_scheduler_commands': '',
     'max_wallclock_seconds': 1 * 60 * 60
 }
