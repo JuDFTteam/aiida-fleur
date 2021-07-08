9ada19ee3c4cff4b24c954676aab49dd52e21e64
janssenhenning <henning.janssen@gmx.net>
2021-07-08 14:17:25 +0200

    Ignore warnings about develop version input files in fleurinp test

--- tests/data/test_fleurinp.py
+++ tests/data/test_fleurinp.py
@@ -51,7 +51,17 @@ def test_fleurinp_valid_inpxml(create_fleurinp, inpxmlfilepath):
     fleurinp_tmp = create_fleurinp(inpxmlfilepath)
 
     assert fleurinp_tmp.inp_dict != {}
-    assert fleurinp_tmp.parser_info['parser_warnings'] == []
+
+    parser_warnings = fleurinp_tmp.parser_info['parser_warnings'].copy()
+
+    if any("Schema available for version" in warning for warning in parser_warnings):
+        for warning in parser_warnings.copy():
+            if 'Input file does not validate against the schema' in warning:
+                parser_warnings.remove(warning)
+            if 'Schema available for version' in warning:
+                parser_warnings.remove(warning)
+
+    assert parser_warnings == []
     assert fleurinp_tmp._validate() is None  # if fails, _validate throws an error
 
 
