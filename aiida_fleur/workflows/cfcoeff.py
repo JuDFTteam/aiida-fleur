7383f0498c5b6197c47073fe53561441942cc960
janssenhenning <henning.janssen@gmx.net>
2022-02-05 14:07:13 +0100

    Extract parameters from fleurinp for analogue

--- aiida_fleur/workflows/cfcoeff.py
+++ aiida_fleur/workflows/cfcoeff.py
@@ -192,6 +192,8 @@ class FleurCFCoeffWorkChain(WorkChain):
 
         if 'calc_parameters' in input_scf:
             rare_earth_params = input_scf['calc_parameters'].get_dict()
+        elif 'fleurinp' in input_scf:
+            rare_earth_params = input_scf['fleurinp'].get_parameter_data_ncf(write_ids=False).get_dict()
         else:
             rare_earth_params = {}
 
