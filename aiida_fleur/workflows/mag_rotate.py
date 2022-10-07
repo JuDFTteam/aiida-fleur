7f4a2c6bd2ebe4c06be86544b58102cadeefe565
janssenhenning <henning.janssen@gmx.net>
2022-10-07 15:56:11 +0200

    updates

--- aiida_fleur/workflows/mag_rotate.py
+++ aiida_fleur/workflows/mag_rotate.py
@@ -17,7 +17,7 @@ class FleurMagRotateWorkChain(WorkChain):
     Workchain for calculating different magnetic directions
     both for second variation SOC and noco
     """
-    _workflowversion = '0.0.2'
+    _workflowversion = '0.1.0'
 
     _wf_default = {
         'angles': [],  #[(0.0,0.0), (np.pi/4, 0.0), ...]
@@ -41,7 +41,8 @@ class FleurMagRotateWorkChain(WorkChain):
 
         spec.output('output_mag_rotate_wc_para', valid_type=orm.Dict, required=True)
 
-        spec.exit_code(400, 'ERROR_SUBPROCESS_FAILED', message='Some configurations failed')
+        spec.exit_code(400, 'ERROR_SOME_DIRECTIONS_FAILED', message='Some configurations failed')
+        spec.exit_code(320, 'ERROR_ALL_DIRECTIONS_FAILED', message='All configurations failed')
 
     @classmethod
     def get_builder_continue(cls, node):
@@ -59,6 +60,17 @@ class FleurMagRotateWorkChain(WorkChain):
             if not re.fullmatch(r'scf\_[0-9]+', link.link_label):
                 continue
             builder.remotes[link.link_label] = link.node.outputs.last_calc.remote_folder
+        
+        #Remove all the different starting configurations
+        #to start from the remote folders
+        if 'fleurinp' in builder.scf:
+            del builder.scf.fleurinp
+        if 'structure' in builder.scf:
+            del builder.scf.structure
+            del builder.scf.inpgen
+        if 'calc_parameters' in builder.scf:
+            del builder.scf.calc_parameters
+
         return builder
 
     def start(self):
@@ -74,7 +86,7 @@ class FleurMagRotateWorkChain(WorkChain):
 
         self.ctx.run_sequentially = False
         self.ctx.current_configuration = 0
-        self.ctx.successful = False
+        self.ctx.successful = True
         self.ctx.info = []
         self.ctx.warnings = []
         self.ctx.errors = []
@@ -211,6 +223,7 @@ class FleurMagRotateWorkChain(WorkChain):
 
         distancelist = []
         t_energylist = []
+        t_energylist_all = []
         outnodedict = {}
         n_configurations = len(self.ctx.wf_dict['angles'])
         e_u = 'eV'
@@ -220,37 +233,47 @@ class FleurMagRotateWorkChain(WorkChain):
 
             calc = self.ctx[label]
 
-            if not calc.is_finished_ok:
-                message = f'One SCF workflow was not successful: {label}'
-                self.ctx.warnings.append(message)
-                self.ctx.successful = False
-                continue
-
             try:
                 outputnode_scf = calc.outputs.output_scf_wc_para
             except KeyError:
                 message = f'One SCF workflow failed, no scf output node: {label}. I skip this one.'
                 self.ctx.errors.append(message)
+                t_energylist_all.append(None)
+                t_energylist.append(None)
+                distancelist.append(None)
                 self.ctx.successful = False
                 continue
+
+            if not calc.is_finished_ok:
+                message = f'One SCF workflow was not successful: {label}'
+                self.ctx.warnings.append(message)
+                self.ctx.successful = False
+                if calc.exit_status not in FleurScfWorkChain.get_exit_statuses(['ERROR_DID_NOT_CONVERGE']):
+                    t_energylist_all.append(None)
+                    t_energylist.append(None)
+                    distancelist.append(None)
+                    continue
 
             outnodedict[label] = outputnode_scf
 
             outpara = outputnode_scf.get_dict()
 
-            t_e = outpara.get('total_energy', float('nan'))
+            t_e = outpara.get('total_energy', None)
             e_u = outpara.get('total_energy_units', 'eV')
             if e_u in ['Htr', 'htr']:
                 t_e = t_e * HTR_TO_EV
-            dis = outpara.get('distance_charge', float('nan'))
+            dis = outpara.get('distance_charge', None)
             dis_u = outpara.get('distance_charge_units', 'me/bohr^3')
-            t_energylist.append(t_e)
+            if calc.is_finished_ok:
+                t_energylist.append(t_e)
+            t_energylist_all.append(t_e)
             distancelist.append(dis)
 
         out = {
             'workflow_name': self.__class__.__name__,
             'workflow_version': self._workflowversion,
             'angles': self.ctx.wf_dict['angles'],
+            'total_energy_including_non_converged': t_energylist_all,
             'total_energy': t_energylist,
             'total_energy_units': e_u,
             'distance_charge': distancelist,
@@ -284,6 +307,10 @@ class FleurMagRotateWorkChain(WorkChain):
         for link_name, node in returndict.items():
             self.out(link_name, node)
 
+        if all(e is None for e in t_energylist):
+            return self.exit_codes.ERROR_ALL_DIRECTIONS_FAILED
+        if not self.ctx.successful:
+            return self.exit_codes.ERROR_SOME_DIRECTIONS_FAILED
 
 @cf
 def create_mag_rotate_result_node(**kwargs):
