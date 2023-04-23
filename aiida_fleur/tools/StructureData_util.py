1d60f209bbc502ad4c6e0598be506400d59642c0
Robin Hilgers <87805664+RobinHilg@users.noreply.github.com>
2023-04-23 18:02:22 +0200

    Rework also here

--- aiida_fleur/tools/StructureData_util.py
+++ aiida_fleur/tools/StructureData_util.py
@@ -1542,9 +1542,12 @@ def adjust_sym_film_relaxation(structure,
             rebuilt_structure.append_atom(symbols=atom[1], position=(atom[0][0], atom[0][1], atom[0][2]), name=atom[1])
 
     prev_distance = 0
-    if len(ILD.keys())>0:
+    if ILD!=None:
+        if len(ILD.keys())>0:
     #Init Counting
-        keyILD=list(ILD)#List of keys
+            keyILD=list(ILD)#List of keys
+    else:
+        keyILD=0
     #Now iterate over all other layers
     for i, layer in enumerate(sorted_layers[1:]):
         layer_copy = deepcopy(layer)
@@ -1564,7 +1567,7 @@ def adjust_sym_film_relaxation(structure,
         prev_layer_z = max(x.position[2] for x in rebuilt_structure.sites)
 
         for atom in layer_copy:
-            if len(ILD.keys())<=0:
+            if ILD==None:
                 atom[0][2] = prev_layer_z + prev_distance
             else:
                 if ILD[keyILD[i]] == 0.0:
